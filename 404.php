2a6e918d6dabd38ac6e6ec325af507193cf863d4
shawn20809 <sdearmitt31@southhills.edu>
2020-01-24 02:45:25 -0500

    404

--- 404.php
+++ 404.php
@@ -19,37 +19,12 @@ get_header();
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<!-- <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'uppercase' ); ?></p> -->
 					
 					<img class="404-img" src="<?php bloginfo('template_directory');?>/assets/images/empty-pockets.jpg" alt="">
 
+					<p>Would you like to try searching fo something else?</p>
 					<?php
 					get_search_form();
-
-					// the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<!-- <div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'uppercase' ); ?></h2>
-						<ul>
-							<?php
-							// wp_list_categories( array(
-							// 	'orderby'    => 'count',
-							// 	'order'      => 'DESC',
-							// 	'show_count' => 1,
-							// 	'title_li'   => '',
-							// 	'number'     => 10,
-							// ) );
-							?>
-						</ul>
-					</div> -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$uppercase_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'uppercase' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$uppercase_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
 					?>
 
 				</div><!-- .page-content -->
