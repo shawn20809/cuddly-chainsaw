7849ef52100b92ed02fbeeed37998578793fba8f
shawn20809 <sdearmitt31@southhills.edu>
2020-01-24 02:26:09 -0500

    404

--- 404.php
+++ 404.php
@@ -15,11 +15,13 @@ get_header();
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'uppercase' ); ?></h1>
+					<h1 class="page-title">Looks like that wasnt right</h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'uppercase' ); ?></p>
+					<!-- <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'uppercase' ); ?></p> -->
+					
+					<img class="what-ever" src="<?php bloginfo('template_directory'); ?> /assests/images/empty-pockets.jpg" alt="">
 
 					<?php
 					get_search_form();
